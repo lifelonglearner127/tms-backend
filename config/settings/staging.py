dbe0e68ca84bc5b42dfe0d308758edb4dc8c09c6
lifelonglearner127 <life.long.learner127@outlook.com>
2019-05-22 02:45:41 +0800

    update staging setting

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -11,7 +11,9 @@ SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
     default="i1t=60pmfy@f2yloh#mzj7%)#jtgll(3wb@@@23w=l(ai&!q1%",
 )
-ALLOWED_HOSTS = ['tms-heroku.herokuapp.com']
+ALLOWED_HOSTS = [
+    'tms-heroku.herokuapp.com', 'tms-frontend-heroku.herokuapp.com'
+]
 
 os.makedirs(str(APPS_DIR.path("static")), exist_ok=True)
 django_heroku.settings(locals())
