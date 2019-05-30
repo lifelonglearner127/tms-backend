13d1ae5026dfe74e197336c32fdde2430aeaf9ec
lifelonglearner127 <life.long.learner127@outlook.com>
2019-05-30 22:28:28 +0800

    add alibaba staing config

---
+++ config/settings/staging_alibaba.py
@@ -0,0 +1,17 @@
+from .base import *  # noqa
+from .base import env
+
+
+# General
+# ----------------------------------------------------------------------------
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="i1t=60pmfy@f2yloh#mzj7%)#jtgll(3wb@@@23w=l(ai&!q1%",
+)
+ALLOWED_HOSTS = [
+    '0.0.0.0'
+]
+
+CORS_ORIGIN_WHITELIST = [
+    '47.98.111.251'
+]
