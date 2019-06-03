7c2d93ac4610970c84c9ffbce15b1a59e0115d40
lifelonglearner127 <life.long.learner127@outlook.com>
2019-06-04 07:00:19 +0800

    add staticfiles folder

--- config/settings/base.py
+++ config/settings/base.py
@@ -140,7 +140,6 @@ AUTH_PASSWORD_VALIDATORS = [
 BASE_DIR = str(ROOT_DIR)
 STATIC_ROOT = str(ROOT_DIR("staticfiles"))
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [str(APPS_DIR.path("static"))]
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
