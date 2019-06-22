89cc4cebbbd65d06d7bcd445a4e756739bf80cb4
lifelonglearner127 <life.long.learner127@outlook.com>
2019-06-23 01:10:15 +0800

    update nginx conf

--- tms/finance/views.py
+++ tms/finance/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404 
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
