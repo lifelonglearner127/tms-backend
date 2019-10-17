a970980ace8dfa236c2fd9118653c091c10df67e
lifelonglearner127 <life.long.learner127@outlook.com>
2019-10-17 14:23:33 +0800

    update event serializer

--- tms/notification/serializers.py
+++ tms/notification/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from . import models as m
+from ..core import constants as c
+from ..core.serializers import TMSChoiceField
 from ..account.serializers import UserNameSerializer
 
 
@@ -21,6 +23,7 @@ class ReadNotificationSerializer(serializers.ModelSerializer):
 
 class EventSerializer(serializers.ModelSerializer):
 
+    event_type = TMSChoiceField(choices=c.EVENT_TYPE)
     vehicle = serializers.SerializerMethodField()
     driver = UserNameSerializer(read_only=True)
 
@@ -30,6 +33,9 @@ class EventSerializer(serializers.ModelSerializer):
 
     def get_vehicle(self, instance):
         ret = {}
+        if instance.vehicle is None:
+            return None
+
         ret['id'] = instance.vehicle.id
         if instance.is_head:
             ret['plate_num'] = instance.vehicle.plate_num
