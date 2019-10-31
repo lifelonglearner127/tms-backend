9e3c35ffa2d5858706d2f3fd1aad076c7eba88a6
lifelonglearner127 <life.long.learner127@outlook.com>
2019-11-01 03:39:53 +0800

    add db backup script

---
+++ backups/pg_backup.sh
@@ -0,0 +1,22 @@
+BACKUP_DIR=/home/dev
+DAYS_TO_KEEP=14
+DATABASE=tms
+USER=dev
+
+FILE="tms_`date +"%Y-%m-%d_%H:%M"`.sql"
+
+OUTPUT_FILE=${BACKUP_DIR}/${FILE}
+
+# do the database backup (dump)
+# use this command for a database server on localhost. add other options if need be.
+pg_dump -U ${USER} ${DATABASE} -F p -f ${OUTPUT_FILE}
+
+# gzip the mysql database dump file
+gzip $OUTPUT_FILE
+
+# show the user the result
+echo "${OUTPUT_FILE}.gz was created:"
+ls -l ${OUTPUT_FILE}.gz
+
+# prune old backups
+find $BACKUP_DIR -maxdepth 1 -mtime +$DAYS_TO_KEEP -name "*${FILE_SUFFIX}.gz" -exec rm -rf '{}' ';'
